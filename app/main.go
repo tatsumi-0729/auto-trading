ab1178329783c6fb7a19bc9c1a9a130a4bac8235
ttamura <ttamura@bltinc.co.jp>
2020-12-07 20:15:02 +0900

    fix

--- app/main.go
+++ app/main.go
@@ -9,7 +9,9 @@ import (
 )
 
 func main() {
+	// log出力の設定
 	util.Logging(config.Config.LogFile)
 	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
 	fmt.Println(apiClient.GetBalance())
+	// fmt.Println(apiClient.GetTicker("ETH_BTC"))
 }
