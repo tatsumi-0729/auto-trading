829d37f7645e8864d66af71b92066a2670ff02ae
ttamura <ttamura@bltinc.co.jp>
2020-12-10 20:11:40 +0900

    fix

--- app/config/config.go
+++ app/config/config.go
@@ -7,12 +7,18 @@ import (
 )
 
 type ConfigList struct {
-	ApiKey        string
-	ApiSecret     string
-	LogFile       string
-	BaseUrl       string
-	GetBalanceUrl string
-	GetTickerUrl  string
+	ApiKey                  string
+	ApiSecret               string
+	LogFile                 string
+	ProductCode             string
+	BaseUrl                 string
+	GetBalanceUrl           string
+	GetTickerUrl            string
+	GetRealTimeTickerHost   string
+	GetRealTimeTickerSchema string
+	GetRealTimeTickerPath   string
+	SendOrderUrl            string
+	ListOrderUrl            string
 }
 
 var Config ConfigList
@@ -23,11 +29,17 @@ func init() {
 		log.Fatalln("fail to load Config", err)
 	}
 	Config = ConfigList{
-		ApiKey:        cfg.Section("bitflyer").Key("api_key").String(),
-		ApiSecret:     cfg.Section("bitflyer").Key("api_secret").String(),
-		LogFile:       cfg.Section("auto-trading").Key("log_file").String(),
-		BaseUrl:       cfg.Section("auto-trading").Key("base_url").String(),
-		GetBalanceUrl: cfg.Section("auto-trading").Key("get_balance_url").String(),
-		GetTickerUrl:  cfg.Section("auto-trading").Key("get_ticker_url").String(),
+		ApiKey:                  cfg.Section("bitflyer").Key("api_key").String(),
+		ApiSecret:               cfg.Section("bitflyer").Key("api_secret").String(),
+		LogFile:                 cfg.Section("auto-trading").Key("log_file").String(),
+		ListOrderUrl:            cfg.Section("auto-trading").Key("list_order_url").String(),
+		BaseUrl:                 cfg.Section("auto-trading").Key("base_url").String(),
+		GetBalanceUrl:           cfg.Section("auto-trading").Key("get_balance_url").String(),
+		GetTickerUrl:            cfg.Section("auto-trading").Key("get_ticker_url").String(),
+		GetRealTimeTickerHost:   cfg.Section("auto-trading").Key("get_realtime_ticker_host").String(),
+		GetRealTimeTickerSchema: cfg.Section("auto-trading").Key("get_realtime_ticker_schema").String(),
+		GetRealTimeTickerPath:   cfg.Section("auto-trading").Key("get_realtime_ticker_path").String(),
+		SendOrderUrl:            cfg.Section("auto-trading").Key("send_order_url").String(),
+		ProductCode:             cfg.Section("auto-trading").Key("product_code").String(),
 	}
 }
